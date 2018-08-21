ac5b6fadaa433765e5d24252b208e14ccfb7b240
NicklasVraa <n.k.vraa@gmail.com>
2018-08-21 14:43:43 +0200

    Auto-update

--- spec/markdown-formatter-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import MarkdownFormatterView from '../lib/markdown-formatter-view';
-
-describe('MarkdownFormatterView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
